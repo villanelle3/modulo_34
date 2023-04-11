f9790418c6631ebdbf99fc86d22f11b642a8d96a
Laura Augusta <laurah.algusta@gmail.com>
2023-04-11 16:59:11 -0300

    test

---
+++ site/home.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Testes para a homepage', () => {
+    it('Deve renderizar 4 vagas', () => {
+        cy.visit("https://calculadoraimc-six.vercel.app/")
+        // cy.get('.lista li').should('have.length', 4)
+        cy.get('.titulo-sm.text-left').first().should('have.text', 'Cálculo IMC')
+        // cy.get('.lista li').last().should('have.text', 'Walk the dog')
+    })
+})
